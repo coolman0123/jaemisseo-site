cb8dd175e22c204f7313a63b2136a7115a2070f1
nwejin <147528674+nwejin@users.noreply.github.com>
2026-01-03 18:16:11 +0900

    next config update: /game/6561

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/game/6561',
+        destination: 'https://minigame6561.vercel.app/',
+      },
+      {
+        source: '/game/6561/:path*',
+        destination: 'https://minigame6561.vercel.app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
